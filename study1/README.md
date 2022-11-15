9e46208e62b1a70667be27fae5af245ac54fefdc
ngh1223 <45031105+ngh1223@users.noreply.github.com>
2022-11-16 00:17:16 +0900

    feat: Mysql, Gradle 로 구성한 JPA 샘플 프로젝트 생성

---
+++ study1/README.md
@@ -0,0 +1,2 @@
+# jpa-study
+IntelliJ, Mysql, Gradle 로 구성한 JPA 샘플 프로젝트
---
+++ study1/src/main/java/com/ngh/Member.java
@@ -0,0 +1,40 @@
+package com.ngh;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="member")
+public class Member {
+
+    @Id
+    private String id;
+    private String name;
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
